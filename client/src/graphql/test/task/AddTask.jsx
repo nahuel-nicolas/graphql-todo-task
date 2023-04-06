880834a24f06b5c4ce1f24997f290ce4407dcd04
nahuel-nicolas <nnahuel012@gmail.com>
2023-04-06 07:38:27 -0300

    Added new TaskService test 'test AddTask with user'

--- client/src/graphql/test/task/AddTask.jsx
+++ client/src/graphql/test/task/AddTask.jsx
@@ -1,54 +1,80 @@
-import { useMutation } from '@apollo/client';
-import { useState } from "react";
+import { useMutation, useQuery } from '@apollo/client';
+import { useEffect, useState } from "react";
 
 import { ADD_TASK } from "../../mutations/taskMutations"
 import { GET_TASKS } from "../../queries/taskQueries";
+import { GET_USERS } from "../../queries/userQueries";
 
 
-export default function AddTask({ taskData }) {
-    const [task, setTask] = useState({
-      title: taskData.title,
-      description: taskData.description,
-      status: taskData.status,
-      userId: taskData.userId
+export default function AddTask({ taskData, userData }) {
+  const { loading, error, data } = useQuery(GET_USERS);
+  const [task, setTask] = useState({
+    title: taskData.title,
+    description: taskData.description,
+    status: taskData.status,
+    userId: taskData.userId
+  })
+
+  useEffect(() => {
+    const userId = getUserId()
+    console.log(['AddTask.useEffect[]', { userId }])
+    setTask({
+      ...task,
+      userId: userId
     })
-  
-    const [addTask] = useMutation(ADD_TASK, {
-      variables: {
-        title: task.title,
-        description: task.description,
-        status: task.status,
-        userId: task.userId
-      },
-      update(cache, { data: { addTask } }) {
-        const cacheData = cache.readQuery({ query: GET_TASKS });
-        const tasks = cacheData?.tasks;
-        if (tasks) {
-          cache.writeQuery({
-            query: GET_TASKS,
-            data: { tasks: [...tasks, addTask] },
-          });
-        }
-      },
-    });
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      addTask(
-        task.title,
-        task.description,
-        task.status,
-        task.userId
-      )
+  }, [])
+
+  const [addTask] = useMutation(ADD_TASK, {
+    variables: {
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      userId: task.userId
+    },
+    update(cache, { data: { addTask } }) {
+      const cacheData = cache.readQuery({ query: GET_TASKS });
+      const tasks = cacheData?.tasks;
+      if (tasks) {
+        cache.writeQuery({
+          query: GET_TASKS,
+          data: { tasks: [...tasks, addTask] },
+        });
+      }
+    },
+  });
+
+  const getUserId = () => {
+    if (userData) {
+      const usersResponseData = data
+      return usersResponseData.users.find(user => user.username === userData.username).id
     }
-  
-    return (
-      <>
-        <p>{task.title}</p>
-        <p>{task.description}</p>
-        <p>{task.status}</p>
-        <p>{task.userId}</p>
-        <button role="submit-button" onClick={handleSubmit}>Add Task</button>
-      </>
+    return null
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(['AddTask.handleSubmit()', { taskData: task }])
+    addTask(
+      task.title,
+      task.description,
+      task.status,
+      task.userId
     )
   }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error)
+    return <p>Something Went Wrong</p>;
+  }
+
+  return (
+    <>
+      <p>{task.title}</p>
+      <p>{task.description}</p>
+      <p>{task.status}</p>
+      <p>{task.userId}</p>
+      <button role="submit-button" onClick={handleSubmit}>Add Task</button>
+    </>
+  )
+}
--- client/src/graphql/test/task/TaskService.test.jsx
+++ client/src/graphql/test/task/TaskService.test.jsx
@@ -1,12 +1,14 @@
 import { render, screen, waitFor, fireEvent, cleanup } from "@testing-library/react";
 import { afterEach, beforeAll, describe, expect, it } from 'vitest'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
-import { ApolloProvider, ApolloClient, InMemoryCache, useQuery, useMutation } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import AddTask from "./AddTask";
 import GetTasks from "./GetTasks";
 import UpdateTask from "./UpdateTask";
 import DeleteTask from "./DeleteTask";
+import AddUser from "../user/AddUser";
+import DeleteUser from "../user/DeleteUser";
 
 
 const cache = new InMemoryCache({
@@ -147,4 +149,104 @@ describe('test DeleteTask', () => {
     })
   })
 })
-cleanup()
+
+const addUserData = {
+  username: 'usernameAssignedToTaskTest',
+  password: 'hi1234'
+}
+
+const addTaskWithUser = {
+  title: 'addTaskWithUserTest',
+  description: 'Add task with user test description',
+  status: 'new',
+  userId: null
+}
+
+describe('test AddTask with user', () => {
+  beforeAll(() => {
+    cleanup()
+  })
+
+  it('should render AddUser', () => {
+    const ProvidedAddUser = (
+      <ApolloProvider client={client}>
+          <Routes>
+              <Route path='/' element={
+                <AddUser addUserData={addUserData} />
+              } />
+          </Routes>
+      </ApolloProvider>
+    )
+    render(ProvidedAddUser, { wrapper: MemoryRouter })
+  })
+
+  it('should wait for addUser compund and submit user create request', async () => {
+    await waitFor(() => {
+      const submitButton = screen.getByRole('submit-button')
+      submitButton.click() 
+    })
+  })
+
+  it('should render AddTask', () => {
+    cleanup()
+    const ProvidedAddTask = (
+      <ApolloProvider client={client}>
+          <Routes>
+              <Route path='/' element={
+                <AddTask taskData={addTaskWithUser} userData={addUserData} />
+              } />
+          </Routes>
+      </ApolloProvider>
+    )
+    render(ProvidedAddTask, { wrapper: MemoryRouter })
+  })
+
+  it('should wait for addTask compund and then submit addTaskData with user', async () => {
+    await waitFor(() => {
+      const submitButton = screen.getByRole('submit-button')
+      submitButton.click()
+    })
+  })
+
+  it('should render DeleteTask', () => {
+    cleanup()
+    const ProvidedDeleteTask = (
+        <ApolloProvider client={client}>
+            <Routes>
+                <Route path='/' element={
+                  <DeleteTask deleteTaskData={addTaskWithUser} />
+                } />
+            </Routes>
+        </ApolloProvider>
+    )
+    render(ProvidedDeleteTask, { wrapper: MemoryRouter })
+  })
+
+  it('should wait for DeleteTask to fetch tasks', async () => {
+    await waitFor(() => {
+      screen.getByRole('delete-task-button').click()
+      // screen.debug()
+    })
+  })
+
+  it('should render DeleteUser', () => {
+    cleanup()
+    const ProvidedDeleteUser = (
+        <ApolloProvider client={client}>
+            <Routes>
+                <Route path='/' element={
+                  <DeleteUser deleteUserData={addUserData} />
+                } />
+            </Routes>
+        </ApolloProvider>
+    )
+    render(ProvidedDeleteUser, { wrapper: MemoryRouter })
+  })
+
+  it('should wait for DeleteUser to fetch users', async () => {
+    await waitFor(() => {
+      screen.getByRole('delete-user-button').click()
+      // screen.debug()
+    })
+  })
+})
