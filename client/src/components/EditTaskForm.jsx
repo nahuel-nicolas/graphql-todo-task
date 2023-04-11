b91ce2bc1fc11e7a8724acd82649d2704c4104bb
nahuel-nicolas <nnahuel012@gmail.com>
2023-04-11 05:25:53 -0300

    Added client.src.components.EditTaskForm

---
+++ client/src/components/EditTaskForm.jsx
@@ -0,0 +1,166 @@
+import { useEffect, useState, useCallback } from "react"
+import { useQuery, useMutation } from "@apollo/client"
+import { useNavigate } from 'react-router-dom'
+import { Select, Form, Button } from 'semantic-ui-react';
+
+import { GET_TASK, GET_TASKS } from "../graphql/queries/taskQueries"
+import { GET_USERS } from "../graphql/queries/userQueries"
+import { UPDATE_TASK, DELETE_TASK } from "../graphql/mutations/taskMutations"
+import { statusOptions, initUserOptions } from "../utils/options"
+import { getOptionsFromApolloUseQueryResponse } from "../utils/utils"
+
+
+export default function EditTaskForm({ taskId }) {
+    const navigateTo = useNavigate()
+    const [userOptions, setUserOptions] = useState([])
+    const [task, setTask] = useState({
+        id: '',
+        title: '',
+        description: '',
+        status: '',
+        userId: ''
+    })
+
+    // as per https://github.com/apollographql/react-apollo/issues/4008 recomends using useCallback
+    // const onCompletedGetUsers = useCallback((data) => {
+    //     console.log(['EditTaskForm.onCompletedGetUsers', data])
+    //     if (data) {
+    //         setUserOptions([
+    //             ...initUserOptions,
+    //             ...getOptionsFromApolloUseQueryResponse(data.users, 'id', 'username')
+    //         ])
+    //     }
+    // }, [])
+    // const onCompletedGetTask = useCallback((data) => {
+    //     console.log(['EditTaskForm.onCompletedGetTask', data])
+    //     if (data) {
+    //         setTask(data.task)
+    //     }
+    // }, [])
+
+    const onCompletedGetUsers = (data) => {
+        console.log(['EditTaskForm.onCompletedGetUsers', data])
+        if (data) {
+            setUserOptions([
+                ...initUserOptions,
+                ...getOptionsFromApolloUseQueryResponse(data.users, 'id', 'username')
+            ])
+        }
+    }
+    const onCompletedGetTask = (data) => {
+        console.log(['EditTaskForm.onCompletedGetTask', data])
+        if (data) {
+            setTask(data.task)
+        }
+    }
+    
+    const { loagin: usersLoading, error: usersError, data: users } = useQuery(
+        GET_USERS, { 
+            onCompleted: onCompletedGetUsers
+        }
+    );
+
+    const { loagin: taskLoading, error: taskError, data: taskData } = useQuery(
+        GET_TASK, 
+        { 
+            variables: { id: taskId }, 
+            onCompleted: onCompletedGetTask
+        }
+    );
+
+    const [deleteTask] = useMutation(DELETE_TASK, {
+        variables: { id: task.id },
+        refetchQueries: [{ query: GET_TASKS }],
+    });
+    const [updateTask] = useMutation(UPDATE_TASK, {
+        variables: { 
+          id: task.id, 
+          title: task.title,
+          description: task.description,
+          status: task.status,
+          userId: task.userId
+        },
+        refetchQueries: [{ query: GET_TASK, variables: { id: task.id } }],
+    });
+
+    const handleChange = event => {
+        setTask({
+            ...task,
+            [event.target.name]: event.target.value
+        });
+    };
+    
+    const handleSelectChange = (event, props) => {
+        setTask({
+            ...task,
+            [props.name]: props.value
+        });
+    };
+
+    const handleBack = async (e) => {
+        e.preventDefault()
+        navigateTo('/')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        updateTask(
+            task.id, 
+            task.title, 
+            task.description, 
+            task.status, 
+            task.userId
+        )
+    }
+    
+    const handleDelete = async (e) => {
+        e.preventDefault()
+        deleteTask(task.id) 
+    }
+
+    if (usersLoading || taskLoading) return <p>Loading...</p>;
+    if (usersError || taskError) {
+        console.error(usersError || taskError)
+        return <p>Something Went Wrong</p>;
+    }
+
+    return (
+        <Form>
+            <Form.Input 
+                label='title' 
+                name='title'
+                role='title-input' 
+                value={task.title} 
+                onChange={handleChange} 
+            />
+            <Form.TextArea 
+                label='descripton' 
+                name='description'
+                role='description-textarea'   
+                value={task.description}
+                onChange={handleChange} 
+            />
+            <Select
+                label='Status' 
+                placeholder='Select task status' 
+                name='status'
+                data-testid='status-select'  
+                options={statusOptions} 
+                value={task.status}
+                onChange={handleSelectChange} 
+            />
+            <Select 
+                placeholder='Assign a user' 
+                name='userId'
+                data-testid='user-select'  
+                options={userOptions} 
+                value={task.userId}
+                onChange={handleSelectChange} 
+            />
+            <Button type='submit' onClick={handleBack} role='back-button'>Back</Button>
+            <Button type='submit' onClick={handleSubmit} role='submit-button'>Submit</Button>
+            <Button type='submit' onClick={handleDelete} role='delete-button'>Delete</Button>
+        </Form>
+    )
+}
---
+++ client/src/components/EditTaskForm.test.jsx
@@ -0,0 +1,222 @@
+import { useState, useEffect } from "react";
+import { render, screen, fireEvent, within, waitFor } from "@testing-library/react";
+import { describe, it, expect, beforeAll, afterAll, vi } from "vitest";
+import * as apolloClient from '@apollo/client';
+import * as ReactRouterDom from "react-router-dom";
+
+import EditTaskForm from "./EditTaskForm";
+import { statusOptions } from "../utils/options";
+import { getQueryName } from "../utils/utils";
+
+
+const { ApolloProvider, ApolloClient, InMemoryCache } = apolloClient
+const { MemoryRouter, Routes, Route } = ReactRouterDom
+
+const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          clients: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+          projects: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+});
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3002/graphql',
+    cache
+});
+
+const initTaskData = {
+    id: 'taskId123',
+    title: 'testEditTaskForm',
+    description: 'test edit task form description',
+    status: 'new',
+    userId: 'userId123'
+}
+
+const finalTaskData = {
+    id: 'taskId123',
+    title: 'testEditTaskForm Final',
+    description: 'test edit task form description final',
+    status: 'completed',
+    userId: 'userId456'
+}
+
+const users = [
+    {
+        id: 'userId123',
+        username: 'username1'
+    },
+    {
+        id: 'userId456',
+        username: 'username2'
+    },
+    {
+        id: 'userId789',
+        username: 'username3'
+    }
+]
+
+const initStatusText = statusOptions.find(statusOption => statusOption.value === initTaskData.status).text
+const initUsername = users.find(user => user.id === initTaskData.userId).username
+const finalStatusText = statusOptions.find(statusOption => statusOption.value === finalTaskData.status).text
+const finalUsername = users.find(user => user.id === finalTaskData.userId).username
+
+let goBackButtonClicked = false
+let submitButtonClicked = false
+let deleteButtonClicked = false
+
+function useQueryMock(query, options) {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        const queryName = getQueryName(query)
+        console.log(['EditTaskForm.test.useQueryMock.useEffect[]', queryName])
+
+        if (queryName === 'getUsers') {
+            setData({ users })
+        } else {
+            setData({ task: initTaskData })
+        }
+        setLoading(false)
+    }, [])
+    useEffect(() => {
+        options.onCompleted(data)
+    }, [data])
+    
+    return {
+        loading,
+        error,
+        data
+    }
+}
+
+function useMutationMock(mutation) {
+    const mutationName = getQueryName(mutation)
+    if (mutationName === 'UpdateTask') {
+        return [
+            async (id, title, description, status, userId) => {
+                submitButtonClicked = true
+                const requestDataTask = { id, title, description, status, userId }
+                console.log(['EditTaskForm.useMutationMock.UpdateTask', requestDataTask])
+                expect(requestDataTask).toEqual(finalTaskData)
+                return requestDataTask
+            }
+        ]
+    }
+    return [
+        async (id) => {
+            deleteButtonClicked = true
+            console.log(['EditTaskForm.useMutationMock.DeleteTask', id])
+            expect(id).toEqual(finalTaskData.id)
+            return finalTaskData
+        }
+    ]
+}
+
+function useNavigateMock() {
+    return (navigateToUrl) => {
+        goBackButtonClicked = true
+    }
+}
+
+vi.mock('react-router-dom', async (importOriginal) => {
+    const reactRouterDom = await importOriginal()
+    return {
+        ...reactRouterDom, // since you still want to use the actual MemoryRouter
+        useNavigate: useNavigateMock
+    }
+})
+
+describe('test EditTaskForm', () => {
+    beforeAll(() => {
+        vi.spyOn(apolloClient, 'useQuery').mockImplementation(useQueryMock)
+        vi.spyOn(apolloClient, 'useMutation').mockImplementation(useMutationMock)
+        // vi.spyOn(ReactRouterDom, 'useNavigate').mockImplementation(useNavigateMock)
+    })
+
+    afterAll(() => {
+        vi.restoreAllMocks();
+    })
+
+    it('should render', () => {
+        const ProvidedEditTaskForm = (
+            <ApolloProvider client={client}>
+                <Routes>
+                    <Route path='/' element={
+                        <EditTaskForm taskId={initTaskData.id} />
+                    } />
+                </Routes>
+            </ApolloProvider>
+        )
+        render(ProvidedEditTaskForm, { wrapper: MemoryRouter })
+    })
+
+    it('should wait component compound', async () => {
+        await waitFor(() => {
+            
+        })
+    })
+
+    it('should be able to get initTaskData from screen', () => {
+        expect(screen.getByRole('title-input').defaultValue).toEqual(initTaskData.title)
+
+        expect(screen.getByRole('description-textarea').defaultValue).toEqual(initTaskData.description)
+
+        const statusSelect = screen.getByTestId('status-select')
+        expect(within(statusSelect).getByRole('alert').textContent).toEqual(initStatusText)
+
+        const userSelect = screen.getByTestId('user-select')
+        expect(within(userSelect).getByRole('alert').textContent).toEqual(initUsername)
+    })
+
+    it('should type title and description inputs', () => {
+        const titleInput = screen.getByRole('title-input')
+        const descriptionInput = screen.getByRole('description-textarea')
+        fireEvent.change(titleInput, { target: { value: finalTaskData.title } })
+        fireEvent.change(descriptionInput, { target: { value: finalTaskData.description } })
+    })
+
+    it('should change status', () => {
+        const statusSelect = screen.getByTestId('status-select')
+        const options = within(statusSelect).getAllByRole('option')
+        expect(Array.isArray(options)).toBeTruthy()
+        options.find(option => option.textContent === finalStatusText).click()
+    })
+
+    it('should change user', () => {
+        const userSelect = screen.getByTestId('user-select')
+        const options = within(userSelect).getAllByRole('option')
+        expect(Array.isArray(options)).toBeTruthy()
+        options.find(option => option.textContent === finalUsername).click()
+    })
+
+    it('should go back', () => {
+        expect(goBackButtonClicked).toBeFalsy()
+        screen.getByRole('back-button').click()
+        expect(goBackButtonClicked).toBeTruthy()
+    })
+
+    it('should submit changes', () => {
+        expect(submitButtonClicked).toBeFalsy()
+        screen.getByRole('submit-button').click()
+        expect(submitButtonClicked).toBeTruthy()
+    })
+
+    it('should delete task', () => {
+        expect(deleteButtonClicked).toBeFalsy()
+        screen.getByRole('delete-button').click()
+        expect(deleteButtonClicked).toBeTruthy()
+    })
+})
